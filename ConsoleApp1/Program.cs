c2a059d01951e164f87812a8d9d768bc38971d17
Lucas Teles <lucas.teles@outlook.com>
2021-04-10 20:03:34 -0300

    WIP

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+
+// Console.Beep(52,2 ); // only windows
+
+var volume = .5f;
+
+var step = .05f;
+var wave = Enumerable
+    .Range(0, 48000)
+    .Select(x => x * step)
+    .Select(x => (float)Math.Sin(x))
+    .Select(x => x * volume);
+
+var bytes = wave.SelectMany(BitConverter.GetBytes).ToArray();
+var filename = "./output.bin";
+
+
+
+if (File.Exists(filename)) File.Delete(filename);
+await File.WriteAllBytesAsync(filename,bytes);
+Process.Start("ffplay"," -f f32le -ar 48000 ./output.bin");
