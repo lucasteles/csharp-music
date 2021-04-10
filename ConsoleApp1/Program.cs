1e954919cf781f7837bd76ab8ac4d7266f75584b
Lucas Teles <lucas.teles@outlook.com>
2021-04-10 20:38:14 -0300

    WIP

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
 using System.IO;
+using Seconds = System.Single;
+using Samples = System.Single;
+using Pulse = System.Single;
+using Hz = System.Single;
 
 var sampleRate = 48000f;
 var volume = .5f;
@@ -12,18 +15,30 @@ void Play(float[] wave)
     var filename = "./output.bin";
     var bytes = wave.SelectMany(BitConverter.GetBytes).ToArray();
     if (File.Exists(filename)) File.Delete(filename);
-    File.WriteAllBytes(filename,bytes);
-    Process.Start($"ffplay",$"-showmode 1 -f f32le -ar {sampleRate} ./output.bin");
+    File.WriteAllBytes(filename, bytes);
+    Process.Start($"ffplay", $"-showmode 1 -f f32le -ar {sampleRate} ./output.bin");
 }
 
-float[] GetWave(float step, float duration) =>
+Pulse[] GetWave(float step, Seconds duration) =>
     Enumerable
-        .Range(0, (int)(sampleRate * duration))
+        .Range(0, (int) (sampleRate * duration))
         .Select(x => x * step)
-        .Select(x => (float)Math.Sin(x))
+        .Select(x => (float) Math.Sin(x))
         .Select(x => x * volume)
         .ToArray();
 
-var step =(float)(440f * 2 * Math.PI) / sampleRate;
+Pulse[] Freq(Hz hz, Seconds duration)
+{
+    var step = (float) (hz * 2 * Math.PI) / sampleRate;
+    return GetWave(step, duration);
+}
+
 var duration = 2f;
-Play(GetWave(step, duration));
+var wave = new[]
+    {
+        Freq(440f, .5f),
+        Freq(540f, .5f),
+    }
+    .SelectMany(x => x).ToArray();
+
+Play(wave);
